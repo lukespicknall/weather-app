1d683d7a108e62d3c06b5c818d8ce0201241c1bd
Luke Spicknall <lukejspicknall@gmail.com>
2024-04-07 14:53:27 -0400

    adjust assign.js to move it from a array based data model to a key:value object based data model. Employ a factory function that the weather info get pushed into, rather than the array.

--- src/assign.js
+++ src/assign.js
@@ -1,21 +1,36 @@
 import getWeather from './retrieve';
 
-const parseWeather = async (query) => {
-  const weatherPackage = await getWeather(query);
-//   console.log(weatherPackage);
-  const currentCondition = weatherPackage.current.condition.text;
-  const currentTempC = weatherPackage.current.temp_c;
-  const currentTempF = weatherPackage.current.temp_f;
-  const currentHumidity = weatherPackage.current.humidity;
-  const currentWindMph = weatherPackage.current.wind_mph;
+// factory function to create object with only the weather data we are intereted in from fetch
+const Weather = (condition, tempC, tempF, humidity, windMph) => {
+  const currentCondition = condition;
+  const currentTempC = tempC;
+  const currentTempF = tempF;
+  const currentHumidity = humidity;
+  const currentWindMph = windMph;
 
-  const weatherData = [
+  return {
     currentCondition,
     currentTempC,
     currentTempF,
     currentHumidity,
     currentWindMph,
-  ];
+  };
+};
+
+// async function that takes locale param (query) from call in ui.js
+// passes the local param to getWeather in retrieve.js
+// takes the weather package it recieves from api, pulls the data we are interested in
+// creates and returns weatherData object with that info from Weather factory function
+const parseWeather = async (query) => {
+  const weatherPackage = await getWeather(query);
+
+  const condition = weatherPackage.current.condition.text;
+  const tempC = weatherPackage.current.temp_c;
+  const tempF = weatherPackage.current.temp_f;
+  const { humidity } = weatherPackage.current;
+  const windMph = weatherPackage.current.wind_mph;
+
+  const weatherData = Weather(condition, tempC, tempF, humidity, windMph);
 
   return weatherData;
 };
